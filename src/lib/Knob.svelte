53dd5039cd248fe7ef2ac8a73ca9a6811e5419f4
Sage Raflik <neonfuz@gmail.com>
2022-07-11 14:36:44 -0500

    Made value editable by clicking knob

--- src/lib/Knob.svelte
+++ src/lib/Knob.svelte
@@ -1,28 +1,51 @@
 <script>
  import lockDrag from '$lib/lockDrag.js'
- let val = 50;
- let min = 0;
- let max = 100;
  const clamp = (a, b, c) => Math.min(Math.max(a, b), c);
+ let value = 50;
+ let updated = false;
+ let input;
  function updateVal(drag) {
-     val = clamp(min, val+drag, max);
+     if (drag !== 0) {
+         value = clamp(0, value+drag, 100);
+         updated = true;
+     }
+ }
+ function pointerDown(event) {
+     updated = false;
+ }
+ function pointerUp(event) {
+     if (!updated) {
+         console.log('focusing', input);
+         input.focus();
+     }
  }
 </script>
 
-<div class="knob" use:lockDrag={updateVal}>
-    {val}
+<div use:lockDrag="{updateVal}"
+     on:pointerdown="{pointerDown}"
+     on:pointerup="{pointerUp}">
+    <input type="text" bind:value bind:this={input} />
 </div>
 
 <style>
- .knob {
+ div {
      height: 6rem;
      width: 6rem;
-     font-size: 2rem;
      user-select: none;
      background: black;
      color: white;
      display: grid;
      place-items: center;
      border-radius: 50%;
+ }
+ input {
+     user-select: none;
+     pointer-events: none;
+     font-size: 2rem;
+     width: 2em;
+     text-align: center;
+     background: #0000;
+     color: white;
+     border: none;
  }
 </style>
