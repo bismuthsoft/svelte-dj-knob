91c50748c3213deeca4391838918c404943719d1
Sage Raflik <neonfuz@gmail.com>
2022-07-11 21:14:53 -0500

    Added origin fix for firefox drift bug

--- src/lib/lockdrag.js
+++ src/lib/lockdrag.js
@@ -4,17 +4,28 @@ const defaultTotals = () => ({
 });
 export default function lockDrag(elem) {
     let totals = defaultTotals();
+    let origin;
     function pointerDown() {
         elem.requestPointerLock();
     }
     function pointerMove(event) {
-        let { movementY, movementX } = event;
-        let detail = { movementY, movementX };
+        let { movementX, movementY } = event;
+        // For some reason firefox on some systems has an origin of -1 -1
+        // instead of 0 0. This hack assumes the first frame of pointermove is
+        // the origin. An implication of this hack is if the element is clicked
+        // with velocity the pointer continues to move in that direction.
+        //
+        // Possibly related issue: https://github.com/w3c/pointerlock/issues/42
+        origin ??= { movementX, movementY };
+        movementX -= origin.movementX;
+        movementY -= origin.movementY;
+        const detail = { movementX, movementY };
         elem.dispatchEvent(new CustomEvent("lockdrag", { detail }));
-        totals.movementX += movementX;
-        totals.movementY += movementY;
+        totals.movementX += detail.movementX;
+        totals.movementY += detail.movementY;
     }
     function pointerUp() {
+        origin = undefined;
         document.removeEventListener('pointerup', pointerUp);
         document.exitPointerLock();
         elem.dispatchEvent(new CustomEvent("lockdragrelease", { detail: totals }));
