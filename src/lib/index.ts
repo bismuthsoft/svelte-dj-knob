c63415f28e880027f7c43fb131a62108f8dcecae
Sage Raflik <neonfuz@gmail.com>
2022-08-07 23:44:11 -0500

    Improved exports with index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+import MinimalKnob from './MinimalKnob.svelte';
+import Input from './Input.svelte';
+import Options from './Options.svelte';
+import knobdrag from './knobdrag';
+import options from './options';
+
+export default MinimalKnob;
+export {MinimalKnob, Input, Options, knobdrag, options};
