3d739e7b25e82a63ef58dcff06a6453452fc2715
Sage Raflik <neonfuz@gmail.com>
2022-08-07 23:12:45 -0500

    Fixed inputElem nag in console

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
  export let value: number;
- export let inputElem: Element;
+ export let inputElem: Element | undefined; // briefly undefined before mounted
  export let color: string;
  export let format = (value: number) => {
      return Math.floor(value * 100) / 100;
